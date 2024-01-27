b19e6c54a2e4d5e11aa74381e75409d983a3bc7a
도경 <dokyungkim2926@gmail.com>
2024-01-27 23:40:29 +0900

    s

--- src/main/java/ignis/ignis/domain/feed/domain/Count.java
+++ src/main/java/ignis/ignis/domain/feed/domain/Count.java
@@ -13,6 +13,7 @@ import javax.persistence.*;
 public class Count  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id", nullable = false)
