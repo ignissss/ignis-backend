92fa8d060340eb615782e0f24f5f6cbe45dd5d7b
신희성 <vvwv3092@gmail.com>
2024-01-28 11:16:21 +0900

    Update comment

--- src/main/java/ignis/ignis/domain/comment/controller/CommentController.java
+++ src/main/java/ignis/ignis/domain/comment/controller/CommentController.java
@@ -4,10 +4,7 @@ import ignis.ignis.domain.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "comment", description = "댓글")
 @RestController
@@ -18,7 +15,7 @@ public class CommentController {
 
     @Operation(description = "댓글달기")
     @PostMapping("/{feedId}")
-    public void createComment(@PathVariable Long feedId, String comment){
+    public void createComment(@PathVariable Long feedId, @RequestParam String comment){
         commentService.create(feedId, comment);
     }
 }
